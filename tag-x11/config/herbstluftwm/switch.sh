55f1355e05ccbc499b1ca9fb05e5bd9f32566fb1
Philipp Rustemeier <philipp@bytepool.net>
2015-12-09 22:40:58 +0100

    added song switcher

--- tag-x11/config/herbstluftwm/switch.sh
+++ tag-x11/config/herbstluftwm/switch.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-cd ~/.config/herbstluftwm
-python system_setup.py list | rofi -p 'switch screen: ' -width 20 -dmenu | xargs python system_setup.py set
+case "$1" in
+    screen)
+        cd ~/.config/herbstluftwm
+        python system_setup.py list | rofi -p 'switch screen: ' -width 20 -dmenu | xargs python system_setup.py set
 
-# restart topbar if running
-if pidof lemonbar; then
-    killall lemonbar
-    sh topbar.sh &
-fi
+        # restart topbar if running
+        if pidof lemonbar; then
+            killall lemonbar
+            sh topbar.sh &
+        fi
+        ;;
+    song)
+        mpc playlist | rofi -p 'jump to song: ' -dmenu -i --only-match -format d | xargs mpc play
+        ;;
+esac
+
