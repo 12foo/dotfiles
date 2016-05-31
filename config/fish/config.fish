3b9524afef30e572b32c58e0bb7392abf11d1a39
Philipp Rustemeier <philipp@bytepool.net>
2016-05-31 20:22:56 +0200

    fish > conda integration removed

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,8 +6,4 @@ if test -e /usr/share/virtualfish/virtual.fish
     source /usr/share/virtualfish/virtual.fish
 end
 
-if type -q conda
-    source (conda info --root)/bin/conda.fish
-end
-
 eval (dircolors -c ~/.dircolors)
